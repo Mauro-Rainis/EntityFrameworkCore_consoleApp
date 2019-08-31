f60162b71f1367e183f1cbb1cfc5ff9d1b3e73b6
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-31 09:32:03 +0200

    Add project files.

---
+++ EntityFrameworkCore_consoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkCore_consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
