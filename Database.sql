ddcc6a22913d0e1363c01647af2e312c63da6707
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-31 20:36:13 +0200

    Fix db model

--- Database.sql
+++ Database.sql
@@ -1,9 +1,15 @@
-﻿create table studenti(id int primary key, nome varchar(32), sport int);
+﻿create table studenti(
+	id int primary key, 
+	nome varchar(32), 
+	sport int references sport(id));
 insert into studenti values (1,'Mauro',1);
 insert into studenti values (2,'Luca',2);
 insert into studenti values (3,'Anna',NULL);
 
-create table sport(id int, nome varchar(32));
+create table sport(
+	id int primary key, 
+	nome varchar(32)
+	);
 insert into sport values (1,'Mtb');
 insert into sport values (2,'Sci');
 insert into sport values (4,'Danza');
