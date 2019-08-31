33dc61314d080e55404f60fcb692f5d92fa77596
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-31 09:44:14 +0200

    Aggiunto script per la creazione del database di esempio

---
+++ Database.sql
@@ -0,0 +1,9 @@
+﻿create table studenti(id int primary key, nome varchar(32), sport int);
+insert into studenti values (1,'Mauro',1);
+insert into studenti values (2,'Luca',2);
+insert into studenti values (3,'Anna',NULL);
+
+create table sport(id int, nome varchar(32));
+insert into sport values (1,'Mtb');
+insert into sport values (2,'Sci');
+insert into sport values (4,'Danza');
