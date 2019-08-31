0bc5add4b112ae64b57007eb1f74b369c687c7bb
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-31 09:37:58 +0200

    Aggiunto classi Studente e Sport

---
+++ EntityFrameworkCore_consoleApp/Models/Sport.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore_consoleApp.Models
+{
+    class Sport
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
---
+++ EntityFrameworkCore_consoleApp/Models/Studente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore_consoleApp.Models
+{
+    class Studente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public Sport Sport { get; set; }
+    }
+}
